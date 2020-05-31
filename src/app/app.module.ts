e0ce6c58e9e02bc5cdabde86eae5b41e97d74dc4
Ardis Lu <ardisglu@gmail.com>
2020-05-30 20:11:09 -0700

    Add basic site
    Overwrite Angular defaults and begin populating with own content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Angular Core and Routing
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
+// Material Angular
+import { MatToolbarModule, MatGridListModule } from '@angular/material'
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+// User defined components
 import { AppComponent } from './app.component';
+
+const coreModules = [
+  BrowserModule,
+  BrowserAnimationsModule,
+  HttpClientModule,
+  AppRoutingModule
+]
+
+const materialModules = [
+  MatToolbarModule,
+  MatGridListModule,
+  MatCardModule,
+  MatButtonModule,
+  MatIconModule,
+  MatExpansionModule
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    ...coreModules,
+    ...materialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
