7455033d1535137b8d70e4aa1be044e6881d40c3
Ardis Lu <ardisglu@gmail.com>
2020-08-12 20:11:02 -0700

    Add GitHub icon to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,6 @@ export class AppComponent {
 
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon('linkedin', sanitizer.bypassSecurityTrustResourceUrl('../assets/images/linkedin.svg'));
+    iconRegistry.addSvgIcon('github', sanitizer.bypassSecurityTrustResourceUrl('../assets/images/github.svg'));
   }
 }
