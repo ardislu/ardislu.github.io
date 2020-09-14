aca867f03a0c63d9c77abf5bc9df6285bf52bbeb
Ardis Lu <ardisglu@gmail.com>
2020-09-13 17:16:59 -0700

    Add notification dialog shared component

---
+++ src/app/components/notification-dialog/notification-dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface NotificationDialogData {
+  title: string;
+  body: string;
+  buttonText: string;
+}
+
+@Component({
+  selector: 'app-notification-dialog',
+  templateUrl: './notification-dialog.component.html',
+  styleUrls: ['./notification-dialog.component.scss']
+})
+export class NotificationDialogComponent implements OnInit {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: NotificationDialogData) { }
+
+  ngOnInit(): void {
+  }
+
+}
